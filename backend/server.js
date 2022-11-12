28b73e4b7da128b34b3c91600b8d6d48019c2621
Fraan-nz <franco.nnz@hotmail.com>
2022-11-12 18:21:49 -0300

    edit package json

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
+import path from "path";
 
 dotenv.config();
 
@@ -32,6 +33,12 @@ app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 
 app.use("/api/orders", orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) =>
+	res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
+);
 
 app.use((err, req, res, next) => {
 	res.status(500).send({ message: err.message });
