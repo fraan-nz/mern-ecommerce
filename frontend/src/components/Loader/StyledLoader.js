75af436ef8c93cd52fe10e53324b5af6ee260601
Fraan-nz <franco.nnz@hotmail.com>
2022-11-12 22:11:40 -0300

    fixed loader

--- frontend/src/components/Loader/StyledLoader.js
+++ frontend/src/components/Loader/StyledLoader.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const StyledLoader = styled.div`
-	width: 100%;
-	height: calc(100vh - 140px);
 	display: grid;
 	place-content: center;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+
 	.lds-roller {
 		display: inline-block;
 		position: relative;
